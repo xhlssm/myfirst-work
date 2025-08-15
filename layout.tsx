cb125f5911b215c18dd75f532928965c940023cb
WX___JAY <2660691144@qq.com>
2025-08-15 22:52:32 +0800

    Add files via upload

--- layout.tsx
+++ layout.tsx
@@ -1,13 +1,22 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-import './globals.css';
-import ClientRoot from './ClientRoot';
+const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: '绳网终端',
-  description: 'A cyberpunk terminal simulator.',
+export const metadata: Metadata = {
+  title: "绳网终端",
+  description: "A cyberpunk terminal simulator.",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <ClientRoot>{children}</ClientRoot>;
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
 }
-
