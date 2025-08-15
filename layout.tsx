e06dd0bc3da28c2d265ee974628ea996b4788f4e
WX___JAY <2660691144@qq.com>
2025-08-15 18:15:05 +0800

    Add files via upload

--- layout.tsx
+++ layout.tsx
@@ -1,8 +1,9 @@
+
+"use client";
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import DonateAuthor from '@/components/DonateAuthor';
-"use client";
 import React, { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 import SignInPanel from '../components/ui/SignInPanel';
